1e8e56e24197d7a2e5a59573b950740afcfb323e
sevenne <115359166+sevennethescientist@users.noreply.github.com>
2025-02-18 16:36:33 -0500

    added led commands need testing

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -4,17 +4,63 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // TODO -- set this up
 // https://docs.revrobotics.com/rev-crossover-products/blinkin/gs
 
 public class LEDs extends SubsystemBase {
-    /** Creates a new LEDs Subsystem. */
-    public LEDs() {}
+
+    public enum COLORS {
+        kHOTPINK,
+        kCONFETTI,
+        kVIOLET
+    }
+
+    private final Spark m_LEDS;
+    private double led_color;
+
+    public LEDs() {
+        m_LEDS = new Spark(9);
+        led_color = 0.57;
+    }
+
+    public void setColorValue(COLORS color) {
+        switch (color) {
+            case kCONFETTI:
+                led_color = -0.87;
+                break;
+            case kVIOLET:
+                led_color = 0.91;
+                break;
+            case kHOTPINK:
+                led_color = 0.57;
+
+        }
+        //hot pink 0.57
+        //confetti -.87
+        //violet PRETTY .91
+    }
+
+    //TODO TEST THIS MAKE SURE IT DOESNT EXPLODE THE RIO AND DOESNT LAG IT
+    public Command setToViolet() {
+        return this.runOnce( () -> {setColorValue(COLORS.kVIOLET);});
+    }
+
+    public Command setToConfetti() {
+        return this.runOnce( () -> {setColorValue(COLORS.kCONFETTI);} );
+    }
+
+    public Command setToHotPink() {
+        return this.runOnce( () -> {setColorValue(COLORS.kHOTPINK);} );
+    }
+
+
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+       m_LEDS.set(led_color);
     }
 }

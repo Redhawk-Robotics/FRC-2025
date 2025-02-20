67b5148d33d54836b2ebbaf89744ed8cac68eab5
sevenne <115359166+sevennethescientist@users.noreply.github.com>
2025-02-20 14:15:22 -0500

    Added import for Algae? For some reason its miki
    Skibidi pushing

--- src/main/java/frc/robot/subsystems/AlgaeHandler.java
+++ src/main/java/frc/robot/subsystems/AlgaeHandler.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Ports;

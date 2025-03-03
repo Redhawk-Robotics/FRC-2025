5ffdea045585349dc260eb396b435f371c6b85e3
miki <156962108+mikirortiz@users.noreply.github.com>
2025-03-03 16:13:28 -0500

    Started PWM LED code
    Made some methods for solid colors, and blink

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -4,19 +4,95 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 // TODO -- set this up
 // https://docs.revrobotics.com/rev-crossover-products/blinkin/gs
 
+
 public class LEDs extends SubsystemBase {
     /** Creates a new LEDs Subsystem. */
-    AddressableLED m_Led = new AddressableLED(9);
-    public LEDs() {}
+    private final AddressableLED m_led;
+    private final AddressableLEDBuffer m_buffer;
+    private final AddressableLEDBufferView m_left;
+    private final AddressableLEDBufferView m_right;
+    private final LEDPattern m_red;
+    private final LEDPattern m_lightPink;
+    private final LEDPattern m_pink;
+    private final LEDPattern m_rainbow;
+    private final LEDPattern m_violet;
+    private final LEDPattern m_green;
+    private final LEDPattern m_white;
+    private final LEDPattern m_purpleGradient;
+    private final LEDPattern m_redGradient;
+    private final LEDPattern m_greenBase;
+    private final LEDPattern m_greenPattern;
+    private final LEDPattern m_sycned;
 
+
+
+    public LEDs() {
+        this.m_led = new AddressableLED(9);
+        this.m_buffer = new AddressableLEDBuffer(120);
+        m_led.setLength(m_buffer.getLength());
+        m_led.start();
+
+        this.m_left = m_buffer.createView(0, 59);
+        this.m_right = m_buffer.createView(60, 119).reversed();
+
+        //SOlID COLORS
+
+        this.m_lightPink = LEDPattern.solid(Color.kLightPink);
+        this.m_pink = LEDPattern.solid(Color.kPink);
+        this.m_red = LEDPattern.solid(Color.kRed);
+        this.m_violet = LEDPattern.solid(Color.kViolet);
+        this.m_green = LEDPattern.solid(Color.kSpringGreen);
+        this.m_white = LEDPattern.solid(Color.kFloralWhite);
+
+        //NON-SOLID COLORS
+        this.m_rainbow = LEDPattern.rainbow(0, 0);
+
+        this.m_purpleGradient = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, 
+        Color.kViolet, Color.kLavender);
+
+        this.m_redGradient = LEDPattern.gradient(LEDPattern.GradientType.kContinuous,
+        Color.kRed, Color.kCrimson);
+
+        this.m_greenBase = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous,
+        Color.kGreen, Color.kBlack);
+
+        this.m_greenPattern = m_greenBase.blink(Units.Seconds.of(1));  
+
+        this.m_sycned = m_greenBase.synchronizedBlink(RobotController::getRSLState);
+    }  
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        m_led.setData(m_buffer);
+    }
+
+    public void red () {
+        m_red.applyTo(m_buffer);
+        m_led.setData(m_buffer);
+    }
+    
+    public void violet () {
+        m_red.applyTo(m_buffer);
+        m_led.setData(m_buffer);
+    }
+
+    public void greenBlink(){
+        m_greenPattern.applyTo(m_buffer);
+        m_led.setData(m_buffer);
     }
 }
+
+

607b0717a161be5289763be04671cedb6819bbfd
sevenne <115359166+sevennethescientist@users.noreply.github.com>
2025-03-29 16:16:28 -0400

    put my logic back and edited it to have a parellell command stored

--- src/main/java/frc/robot/Commands/CoralPositionFactory.java
+++ src/main/java/frc/robot/Commands/CoralPositionFactory.java
@@ -208,24 +208,33 @@ public final class CoralPositionFactory {
     }
 
 
-    //test moving from feeder to L1
-    // public static Command reworkedStateTestL2(Elevator elevator, Pivot pivot) {
-    //     return Commands.either(
-    //         Commands.sequence( //set the position to safe position
-    //             pivot.runOnce( () -> pivot.setReference(Settings.CoralPosition.PIVOT_MAX_BLOCKING_POSITION)),
-    //             elevator.runOnce( () -> pivot.setReference(Settings.CoralPosition.ELEVATOR_L2_POSITION)),
-    //             pivot.runOnce( () -> pivot.setReference( Settings.CoralPosition.PIVOT_L2_POSITION))
-    //         ),
-    //         pivot.runOnce(() -> pivot.setReference(Settings.CoralPosition.PIVOT_L2_POSITION)),
-    //         () -> elevatorWilBlock(elevator.getPosition(), position.L2.elevPos));
-    //         //lambda changed this boolean to Booleansupplier...?
-    // }
+    // test moving from feeder to L1
+    public static Command reworkedStateTestL2(Elevator elevator, Pivot pivot) {
+        return Commands.either(
+            Commands.sequence( //set the position to safe position
+                pivot.runOnce( () -> pivot.setReference(Settings.CoralPosition.PIVOT_MAX_BLOCKING_POSITION)),
+                Commands.parallel(
+                    pivot.runOnce( () -> pivot.setReference(Settings.CoralPosition.PIVOT_L3_POSITION)),
+                    elevator.runOnce( () -> elevator.setReference(Settings.CoralPosition.ELEVATOR_L3_POSITION))
+                )
+            ),
+            pivot.runOnce(() -> pivot.setReference(Settings.CoralPosition.PIVOT_L2_POSITION)),
+            () -> elevatorWilBlock(elevator.getPosition(), position.L2.elevPos));
+            //lambda changed this boolean to Booleansupplier...?
+    }
 
-    // public static Command reworkedStateTestL3(Elevator elevator, Pivot pivot) {
-    //     return Commands.either(null,
-    //      null,
-    //       null);
-    // }
+    public static Command reworkedStateTestL3(Elevator elevator, Pivot pivot) {
+        return Commands.either(
+            Commands.sequence( //set the position to safe position
+                pivot.runOnce( () -> pivot.setReference(Settings.CoralPosition.PIVOT_MAX_BLOCKING_POSITION)),
+                Commands.parallel(
+                    pivot.runOnce( () -> pivot.setReference(Settings.CoralPosition.PIVOT_L3_POSITION)),
+                    elevator.runOnce( () -> elevator.setReference(Settings.CoralPosition.ELEVATOR_L3_POSITION))
+                )
+            ),
+            pivot.runOnce(() -> pivot.setReference(Settings.CoralPosition.PIVOT_L2_POSITION)),
+            () -> elevatorWilBlock(elevator.getPosition(), position.L2.elevPos));
+    }
 
     // this might be useful?
     // public static Command Stop(Elevator elevator, Pivot pivot) {

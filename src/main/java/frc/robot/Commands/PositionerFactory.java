fc9d7a4d9f0526cd55096708adbd55fdc957d72a
Justin Goodman <jugoodma@users.noreply.github.com>
2025-04-01 07:34:54 -0400

    test tmp

--- src/main/java/frc/robot/Commands/PositionerFactory.java
+++ src/main/java/frc/robot/Commands/PositionerFactory.java
@@ -4,6 +4,9 @@
 
 package frc.robot.Commands;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Stack;
 import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -18,23 +21,6 @@ import frc.robot.subsystems.Pivot;
 public final class PositionerFactory {
 
     private static final class Settings {
-        // TODO tune
-        public static final double ELEVATOR_MIN_BLOCKING_POSITION = 5.; // elevator blocks the pivot above this reference
-        public static final double ELEVATOR_MAX_BLOCKING_POSITION = 55.; // elevator blocks the pivot below this reference
-        public static final double PIVOT_MIN_BLOCKING_POSITION = 120.; // pivot blocks the elevator below this reference
-        public static final double ELEVATOR_ALLOWED_ERROR = 1;
-        public static final double PIVOT_ALLOWED_ERROR = 0.5;
-        public static final double ELEVATOR_FEED_POSITION = 0;
-        public static final double ELEVATOR_L1_POSITION = ELEVATOR_FEED_POSITION;
-        public static final double ELEVATOR_L2_POSITION = ELEVATOR_FEED_POSITION;
-        public static final double ELEVATOR_L3_POSITION = ELEVATOR_FEED_POSITION;
-        public static final double ELEVATOR_L4_POSITION = 106;
-        public static final double PIVOT_FEED_POSITION = 72;
-        public static final double PIVOT_L1_POSITION = 145;
-        public static final double PIVOT_L2_POSITION = 153;
-        public static final double PIVOT_L3_POSITION = 180;
-        public static final double PIVOT_L4_POSITION = 166;
-
         // conflicts:
         // - if elevator is below [threshold_e_s] (near the bottom), then the spoiler and pivot can collide
         //     when spoiler is below [threshold_s_p] and pivot is below [threshold_p_s]
@@ -42,29 +28,45 @@ public final class PositionerFactory {
         // - elevator must be in range [min_e, max_e]
         // - pivot must be in range [min_p, max_p]
         // - spoiler must be in range [min_s, max_s]
-        // TODO tune these!
-        // and then put them into Settings
-        private static final double minPositionWhereElevatorFreesPivotAndSpoiler = 1;
-        private static final double minPositionWherePivotDoesNotCollideWithSpoiler = 10;
-        private static final double minPositionWhereSpoilerDoesNotCollideWithPivot = 1;
-        private static final double minPositionWherePivotDoesNotCollideWithElevator = 10; // threshold_p_e
-        private static final double minPositionWhereElevatorDoesNotBlockPivot = 2; // threshold_e_p1
-        private static final double maxPositionWhereElevatorDoesNotBlockPivot = 20; // threshold_e_p2
-        private static final double midpointPositionWhereElevatorBlocksPivot =
+
+        public static final double minPositionWhereElevatorFreesPivotAndSpoiler = 1;
+        public static final double minPositionWherePivotDoesNotCollideWithSpoiler = 10;
+        public static final double minPositionWhereSpoilerDoesNotCollideWithPivot = 1;
+        public static final double minPositionWherePivotDoesNotCollideWithElevator = 120; // threshold_p_e (pivot blocks the elevator below this reference)
+        public static final double minPositionWhereElevatorDoesNotBlockPivot = 5; // threshold_e_p1 (elevator blocks the pivot above this reference)
+        public static final double maxPositionWhereElevatorDoesNotBlockPivot = 55; // threshold_e_p2 (elevator blocks the pivot below this reference)
+        public static final double midpointPositionWhereElevatorBlocksPivot =
                 (minPositionWhereElevatorDoesNotBlockPivot
                         + maxPositionWhereElevatorDoesNotBlockPivot) / 2;
-        private static final double minElevatorPosition = 0;
-        private static final double maxElevatorPosition = 100;
-        private static final double minPivotPosition = 0;
-        private static final double maxPivotPosition = 10;
-        private static final double minSpoilerPosition = 0;
-        private static final double maxSpoilerPosition = 10;
-        private static final double allowedElevatorError = 1;
-        private static final double allowedPivotError = 1;
-        private static final double allowedSpoilerError = 1;
+
+        public static final double minElevatorPosition = 0;
+        public static final double maxElevatorPosition = 107;
+        public static final double allowedElevatorError = 0.5;
+
+        public static final double minPivotPosition = 70;
+        public static final double maxPivotPosition = 181;
+        public static final double allowedPivotError = 0.5;
+
+        public static final double minSpoilerPosition = 0;
+        public static final double maxSpoilerPosition = 10;
+        public static final double allowedSpoilerError = 0.5;
+
+        public static final double ELEVATOR_FEED_POSITION = 0;
+        public static final double ELEVATOR_L1_POSITION = ELEVATOR_FEED_POSITION;
+        public static final double ELEVATOR_L2_POSITION = ELEVATOR_FEED_POSITION;
+        public static final double ELEVATOR_L3_POSITION = ELEVATOR_FEED_POSITION;
+        public static final double ELEVATOR_L4_POSITION = 106;
+
+        public static final double PIVOT_FEED_POSITION = 72;
+        public static final double PIVOT_L1_POSITION = 145;
+        public static final double PIVOT_L2_POSITION = 153;
+        public static final double PIVOT_L3_POSITION = 180;
+        public static final double PIVOT_L4_POSITION = 166;
     }
 
-    private static class State {
+    static class State {
+        // null means that component in the State is unchanged
+        // between transitions
         Double elevatorSetPoint = null, //
                 pivotSetPoint = null, //
                 coralHandlerSpeed = null, //
@@ -116,7 +118,7 @@ public final class PositionerFactory {
         }
     }
 
-    private static class GoToState extends Command {
+    static class GoToState extends Command {
         private State goal;
         private Stack<State> trajectory;
         private Elevator elevator;
@@ -125,7 +127,7 @@ public final class PositionerFactory {
         private AlgaeHandler algae;
         private AlgaeFloorIntake spoiler;
 
-        public GoToState(State goal, Elevator elevator, Pivot pivot, CoralHandler coral,
+        GoToState(State goal, Elevator elevator, Pivot pivot, CoralHandler coral,
                 AlgaeHandler algae, AlgaeFloorIntake spoiler) {
             this.trajectory = new Stack<>();
             if (goal == null) {
@@ -167,6 +169,131 @@ public final class PositionerFactory {
                     return 3;
                 }
             }
+        }
+
+        static Optional<List<State>> getMidList(State start, State end, Elevator elevator, Pivot pivot) {
+            ArrayList<State> result = new ArrayList<>();
+
+            // Add states to remove conflicts
+            //
+            // How?
+            //
+            // We split into 4 "zones" (see `./positioning-zones.png`)
+            // Rule: the robot can only move between numerically adjacent zones
+            // So, if you are in Z1 and need to get to Z4, then
+            // you have to go to Z2, then to Z3, _then_ to Z4.
+            //
+            // In practice, you might want to go from Feed (Z1) to L4 (Z3)
+            // so you first go to a safe point in Z2 = (goal.pivot, elevator.minBlocking)
+            // and _then_ to L4 = (goal.pivot, goal.elevator).
+            //
+            // You can think of the "zones" as a sort-of "XY" coordinate system
+            // where X == the pivot's horizontal component
+            // and Y == the elevator's vertical component.
+            //
+            // "zones" are highly dependent on the three pivot/elevator
+            // blocking parameters.
+
+            int startZone = zone(start);
+            int endZone = zone(end);
+            if (startZone == 0 || endZone == 0) {
+                DriverStation.reportError(String.format(
+                        "Could not determine State zones for %s->%s (got zones %d->%d)",
+                        start.toString(), end.toString(), startZone, endZone), false);
+                // make this command a no-op
+                return Optional.empty();
+            }
+            int extraStates = Math.abs(startZone - endZone) - 1;
+            if (extraStates > 2) {
+                DriverStation
+                        .reportError(
+                                String.format("Invalid zones for States %s->%s (got zones %d->%d)",
+                                        start.toString(), end.toString(), startZone, endZone),
+                                false);
+                // make this command a no-op
+                return Optional.empty();
+            }
+            if (extraStates == 1) {
+                // curr -> mid -> goal
+                // Z1->Z2->Z3 (or reverse) (Feed -> L4)
+                // Z2->Z3->Z4 (or reverse)
+                // So, we need to know if we're going to Z2 or Z3
+                double pivotPos;
+                double elevatorPos;
+                if (startZone < endZone) {
+                    if (startZone == 1) {
+                        // Z1 -> Z3
+                        // Z2 intermediate (goal.pivot, elevator.minBlocking)
+                        pivotPos = end.pivotSetPoint;
+                        elevatorPos = Settings.minPositionWhereElevatorDoesNotBlockPivot;
+                    } else {
+                        // Z2 -> Z4
+                        // Z3 intermediate (pivot.minBlocking, goal.elevator)
+                        pivotPos = Settings.minPositionWherePivotDoesNotCollideWithElevator;
+                        elevatorPos = end.elevatorSetPoint;
+                    }
+                } else {
+                    if (startZone == 4) {
+                        // Z4 -> Z2
+                        // Z3 intermediate (goal.pivot, elevator.maxBlocking)
+                        pivotPos = end.pivotSetPoint;
+                        elevatorPos = Settings.maxPositionWhereElevatorDoesNotBlockPivot;
+                    } else {
+                        // Z3 -> Z1
+                        // Z2 intermediate (pivot.minBlocking, goal.elevator)
+                        pivotPos = Settings.minPositionWherePivotDoesNotCollideWithElevator;
+                        elevatorPos = end.elevatorSetPoint;
+                    }
+                }
+                BooleanSupplier done = () -> {
+                    return (Math
+                            .abs(pivot.getPosition() - pivotPos) < Settings.allowedPivotError)
+                            && (Math.abs(elevator.getPosition()
+                                    - elevatorPos) < Settings.allowedElevatorError);
+                };
+                State mid = new State(elevatorPos, pivotPos, null, null, end.spoilerSetPoint, null,
+                        done);
+                result.add(mid);
+            } else {
+                // Z1->Z2->Z3->Z4 (or reverse)
+
+                // Z1->Z2 or Z3->Z2
+                // (pivot.minBlocking, elevator.minBlocking)
+                State firstMid = new State(Settings.minPositionWhereElevatorDoesNotBlockPivot,
+                        Settings.minPositionWherePivotDoesNotCollideWithElevator, null, null,
+                        end.spoilerSetPoint, null, () -> {
+                            return (Math.abs(pivot.getPosition()
+                                    - Settings.minPositionWherePivotDoesNotCollideWithElevator) < Settings.allowedPivotError)
+                                    && (Math.abs(elevator.getPosition()
+                                            - Settings.minPositionWhereElevatorDoesNotBlockPivot) < Settings.allowedElevatorError);
+                        });
+
+                // Z2->Z3 or Z4->Z3
+                // (pivot.minBlocking, elevator.maxBlocking)
+                State secondMid = new State(Settings.maxPositionWhereElevatorDoesNotBlockPivot,
+                        Settings.minPositionWherePivotDoesNotCollideWithElevator, null, null,
+                        end.spoilerSetPoint, null, () -> {
+                            return (Math.abs(pivot.getPosition()
+                                    - Settings.minPositionWherePivotDoesNotCollideWithElevator) < Settings.allowedPivotError)
+                                    && (Math.abs(elevator.getPosition()
+                                            - Settings.maxPositionWhereElevatorDoesNotBlockPivot) < Settings.allowedElevatorError);
+                        });
+
+                if (startZone < endZone) {
+                    // Z1->Z2->Z3->Z4
+                    // nothing to do:
+                    // - firstMid = Z1->Z2
+                    // - secondMid = Z2->Z3
+                    result.add(firstMid);
+                    result.add(secondMid);
+                } else {
+                    // Z4->Z3->Z2->Z1
+                    // need to swap firstMid and secondMid
+                    result.add(secondMid);
+                    result.add(firstMid);
+                }
+            }
+            return Optional.of(result);
         }
 
         private State currentGoalState() {
---
+++ src/test/java/PositionerFactoryTest.java
@@ -0,0 +1,18 @@
+package frc.robot.Commands;
+// https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-simulation/unit-testing.html#writing-tests
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import frc.robot.Commands.PositionerFactory;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// TODO test proper creation of trajectory
+
+class PositionerFactoryTest {
+    @Test // marks this method as a test
+    void testTrajectory() {
+        // System.out.println(PositionerFactory.GoToState.getMidList());
+    }
+}

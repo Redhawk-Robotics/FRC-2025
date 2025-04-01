707e22a885436f89f9559c2cbe82fb90e292bb5f
sevenne <115359166+sevennethescientist@users.noreply.github.com>
2025-04-01 17:02:23 -0400

    first round of edits

--- src/main/java/frc/robot/Commands/AutoAlign.java
+++ src/main/java/frc/robot/Commands/AutoAlign.java
@@ -4,28 +4,28 @@
 
 package frc.robot.Commands;
 
+import java.nio.channels.Pipe.SourceChannel;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Vision;
+import frc.robot.subsystems.CoralAligner;
 
 // notes
 // let's turn this into a Factory?
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AutoAlign extends Command {
-  /** Creates a new AutoAlign. */
+  /** Creates a new AutoAlign.  We use time of flight sensors here */
+  
+  private CommandSwerveDrivetrain m_drivetrain;
+  private CoralAligner m_coralAligner;
 
-  //! EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY
-  //TODO CONFIRM
-  private CommandSwerveDrivetrain drivetrain;
-  private Vision vision;
+  public AutoAlign(CommandSwerveDrivetrain m_drivetrain, CoralAligner m_vision) {
+    this.m_drivetrain = m_drivetrain;
+    this.m_coralAligner = m_coralAligner;
 
-  public AutoAlign(CommandSwerveDrivetrain m_drivetrain, Vision m_vision) {
-    this.drivetrain = m_drivetrain;
-    this.vision = m_vision;
-
-    addRequirements(drivetrain, vision);
+    addRequirements(m_drivetrain, m_coralAligner);
   }
 
   // Called when the command is initially scheduled.
@@ -40,11 +40,18 @@ public class AutoAlign extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    System.out.println( "Interrupted, Command Finishing");
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+
+    // if command selected to go left and leftdesired state is true, end
+    //same with other
+    // https://github.com/Redhawk-Robotics/FRC-2024/blob/main/src/main/java/frc/robot/commands/automation/IntakeToShooter.java
+    // Using this becuase it worked prettty well last year 
     return false;
   }
 }
